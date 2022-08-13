1c044e421ca9781383b190e743d8678b2bbc1313
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2022-08-14 08:40:50 +0900

    Add files via upload

---
+++ css/login.js
@@ -0,0 +1,34 @@
+ {
+    font-family: 맑은고딕;
+    font-weight: bold;
+}
+ 
+#wrap {
+    width: 400 px;
+    height: 300 px;
+    border: 1px solid lightskyblue;
+    border-radius: 5px;
+    background-color: skyblue;
+}
+ 
+.loginform {
+    padding: 20px 50px;
+}
+ 
+input {
+    margin-bottom: 14px;
+    width: 290px;
+    height: 30px;
+    font-size: 16px;
+    border: lightgray;
+}
+ 
+button {
+    width: 294px;
+    height: 50px;
+    font-size: 16px;
+    padding:  20px;
+    background-color: #fff;
+    border: 1px solid whitesmoke;
+}
+ 
