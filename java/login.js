ac908f000c4dadcf7938228050d59afa151c286e
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2022-08-14 15:33:22 +0900

    Add files via upload

--- java/login.js
+++ java/login.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 document.querySelector("#btn").addEventListener("click", () => {
     const id = "admin";
     const password = "1234";
@@ -7,7 +5,7 @@ document.querySelector("#btn").addEventListener("click", () => {
     if(id == document.querySelector("#id").value) {
         if(password == document.querySelector("#password").value) {
             alert("업무시작");
-            location.href = ""; //
+            location.href = "gate.html"; 
         }
         else {
             alert("비밀번호가 맞지 않습니다.");
@@ -17,23 +15,3 @@ document.querySelector("#btn").addEventListener("click", () => {
         alert("아이디 혹은 비밀번호가 맞지 않습니다.");
     }
 });
-=======
-
-document.querySelector("#btn").addEventListener("click", () => {
-    const id = "admin";
-    const password = "1234";
- 
-    if(id == document.querySelector("#id").value) {
-        if(password == document.querySelector("#password").value) {
-            alert("업무시작");
-            location.href = ""; //
-        }
-        else {
-            alert("비밀번호가 맞지 않습니다.");
-        }
-    }
-    else {
-        alert("아이디 혹은 비밀번호가 맞지 않습니다.");
-    }
-});
->>>>>>> 42a664f1b8bd5935d345dbfe53e019e02672263b
