037e941b4723734b84baf02a64ca9c39e252ec31
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2022-08-15 14:20:55 +0900

    Update login.js
    Signed-off-by: junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>

--- java/login.js
+++ java/login.js
@@ -1,7 +1,7 @@
 document.querySelector("#btn").addEventListener("click", () => {
     const id = "admin";
     const password = "1234";
-    var link ="myhomepage/gate.html"
+    var link ="html/gate.html"
  
     if(id == document.querySelector("#id").value) {
         if(password == document.querySelector("#password").value) {
