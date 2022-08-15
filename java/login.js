8258601ed6344425238af2222baa828982850d99
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2022-08-15 13:34:57 +0900

    Update login.js
    Signed-off-by: junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>

--- java/login.js
+++ java/login.js
@@ -1,12 +1,12 @@
 document.querySelector("#btn").addEventListener("click", () => {
     const id = "admin";
     const password = "1234";
-    var link ="../gate.html"
+    var link ="/gate.html"
  
     if(id == document.querySelector("#id").value) {
         if(password == document.querySelector("#password").value) {
             alert("업무시작");
-            location.href = link; 
+            window.open(link);
         }
         else {
             alert("비밀번호가 맞지 않습니다.");
