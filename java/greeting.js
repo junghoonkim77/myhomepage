30402c8bec64f045062cfcaaa012934c6a19f078
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2022-08-16 21:41:21 +0900

    Create greeting.js
    Signed-off-by: junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>

---
+++ java/greeting.js
@@ -0,0 +1,15 @@
+    var  yeardaytime= {
+        getYmd10 : function(){
+            var d = new Date();
+            return d.getFullYear() + "-" + ((d.getMonth() + 1) > 9 ? (d.getMonth() + 1).toString() : "0" + (d.getMonth() + 1)) + "-" + 
+            (d.getDate() > 9 ? d.getDate().toString() : "0" + d.getDate().toString());
+            } ,
+        
+         //오늘날짜 중 시분초까지 계산
+            
+         getFullYmdStr : function(){
+                var d = new Date();
+                return d.getFullYear() + "년 " + (d.getMonth()+1) + "월 " + d.getDate() + "일 " + d.getHours() + "시 " + d.getMinutes() + 
+                "분 " + d.getSeconds() + "초 " +  '일월화수목금토'.charAt(d.getUTCDay())+'요일';
+            }
+        }
