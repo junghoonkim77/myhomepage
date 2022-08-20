b288b4bcedb219fe45eb9ef7cff05a3565875a54
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2022-08-20 17:29:38 +0900

    js파일위치이동
    Signed-off-by: junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>

--- java/greeting.js
+++
@@ -1,5 +0,0 @@
-function getFullYmdStr(){
-        var d = new Date();
-        return d.getFullYear() + "y " + (d.getMonth()+1) + "M " + d.getDate() + "DAY " + d.getHours() + "H " + d.getMinutes() + 
-        "분 " + d.getSeconds() + "S " +  '1234567'.charAt(d.getUTCDay())+'week';
-    };
