96eac624bbedd75a3737fb74328f89920fae946d
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2022-08-20 17:05:10 +0900

    날짜함수 재추가
    Signed-off-by: junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>

--- java/login.js
+++ java/login.js
@@ -32,4 +32,6 @@ document.querySelector("#btn").addEventListener("click", () => {
         alert("아이디 혹은 비밀번호가 맞지 않습니다.");
     }
 });
+function getFullYmdStr(){ var d = new Date(); return d.getFullYear() + "년 " + (d.getMonth()+1) + "월 " + d.getDate() + "일 " + d.getHours() + "시 " + d.getMinutes() + "분 " + d.getSeconds() + "초 " + '일월화수목금토'.charAt(d.getUTCDay())+'요일'; };
+function daydiff(){document.getElementById("today_t").innerHTML =getFullYmdStr();}
 
