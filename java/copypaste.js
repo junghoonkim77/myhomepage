6076e23b42917baab3ed5a23c2d0cd305055110f
junghoonkim77 <folkball@gmail.com>
2022-08-21 08:15:35 +0900

    Update copypaste.js

--- java/copypaste.js
+++ java/copypaste.js
@@ -8,6 +8,17 @@ document.getElementById("btn1").onclick = function(){
     document.body.removeChild(textArea);
     alert("오비시간복사");}
 
+//새로운 방법 window.onload  얘는 자스 순서에 맞지 않아도 실행되게 하는 거라네 
 
-
+    window.onload = function () {
+        const valOfDIV = document.querySelector("#btn1");
+    
+        valOfDIV.addEventListener("click", function () {
+            const copyElement = document.querySelector('.h1-clock');
+            copy(copyElement.innerHTML)
+        })
+    }
+       function copy (value) {
+        navigator.clipboard.writeText(value);
+    alert("오비시간복사");}
     
