519d467bb0069ecfdcceb80684dd901833dd6484
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2022-08-20 16:54:13 +0900

    Update login.js
    Signed-off-by: junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>

--- java/login.js
+++ java/login.js
@@ -1,7 +1,5 @@
 var o =["118176867","200867639","118175287","admin"];
 var p =["1234","4321","7890"]
-
-
 Object.prototype.contain = function(needle){
     for( i=0 ; i<o.length ; i++ ){ 
     if(o[i] == needle){
@@ -9,7 +7,6 @@ Object.prototype.contain = function(needle){
     }
     } return  false;
 }
-
 Object.prototype.contain1 = function(needle){
     for( j=0 ; j<o.length ; j++ ){ 
     if(p[j] == needle){
@@ -17,13 +14,10 @@ Object.prototype.contain1 = function(needle){
     }
     } return false ;
 }
-
-
 document.querySelector("#btn").addEventListener("click", () => {
     const id = document.querySelector('#id').value;
     const password = document.querySelector("#password").value;
     var link = "html/gate.html"
-
    
     if(o.contain(id)) {
         if(p.contain1(password)) {
@@ -38,3 +32,4 @@ document.querySelector("#btn").addEventListener("click", () => {
         alert("아이디 혹은 비밀번호가 맞지 않습니다.");
     }
 });
+
