c686880e44e037d8d8f42610805c94f148d913fc
junghoonkim77 <folkball@gmail.com>
2022-08-21 17:43:03 +0900

    Create 1.php

---
+++ code_shelter/php1/1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf=8">
+    </head>
+<body>
+<h1>php</h1>
+  <?php
+      echo 10+45;
+    ?>
+    <h1>javascript</h1>
+    <script>
+ document.write(10+335);
+
+ </script>
+
+
+</body>
+</html>
