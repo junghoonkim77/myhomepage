929c5ebea3c1a0f60f80f44bd9b3061d5917dd3c
junghoonkim77 <folkball@gmail.com>
2022-08-21 17:38:03 +0900

    Create test1.js

---
+++ code_shelter/javascript/test1.js
@@ -0,0 +1,10 @@
+var names = prompt('이름을 입력해주세요');
+
+
+if(names === "김정훈"){ var pwd = prompt('비밀번호를 입력하세요');
+ if( pwd === "1234"){
+    alert('당신이 맞군요');}
+    else{
+        alert('비번이 다른데요?');
+    }} else {alert('누구세요?')};
+
