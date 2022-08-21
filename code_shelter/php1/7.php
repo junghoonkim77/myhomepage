fddf169797d06603acc73a7f43225978d24d8c0b
junghoonkim77 <folkball@gmail.com>
2022-08-21 17:44:18 +0900

    Create 7.php

---
+++ code_shelter/php1/7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf=8">
+    </head>
+<body>
+    <h1>java script</h1>
+    <script charset="utf=8">
+       password = prompt("비밀번호");
+    if(password==1111){
+        document.write("안녕하세요 전 흰둥이에요");
+    } else {
+        document.write("전 김정훈 주인만 따릅니다");
+    }
+    </script>
+
+</body>
+</html>
