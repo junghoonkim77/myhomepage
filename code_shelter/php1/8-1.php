b71ac0e72a4b81bdfc58a30d6aeb286888f84a91
junghoonkim77 <folkball@gmail.com>
2022-08-21 17:44:40 +0900

    Create 8-1.php

---
+++ code_shelter/php1/8-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf=8">
+    </head>
+<body>
+    <form action="8-2.php" method>
+    <p>비밀번호를 입력해주세요</P>
+    <input type="text" name="password">
+    <input type="hidden" name="hide" value="egoing">
+    <input type="submit">
+</form>
+
+    
+
+
+</body>
+</html>
