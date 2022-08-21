f65e507fe938f12f0eaee51235eed85d1ea2af0f
junghoonkim77 <folkball@gmail.com>
2022-08-21 17:44:57 +0900

    Create 8-3.php

---
+++ code_shelter/php1/8-3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf=8">
+<body>
+    <h1>java script</h1>
+    <ol>
+    <script charset="utf=8">
+     i = 0;
+     while (i<10) {
+         document.write("<li>hello world</li>");
+         document.write(i);
+         i = i +1 ;
+        }    
+    </script>
+
+   </ol>
+    <h1> php</h1>
+    <ol>
+    <?php
+     $i = 0;
+     while($i < 10) {
+         echo " <li> hello world </li>";
+         $i = $i +1 ;
+        echo $i;
+
+        }
+    ?>
+    </ol>
+
+</body>
+</html>
