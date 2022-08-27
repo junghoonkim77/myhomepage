9c98a628da66c3f0b64d893675b8fdf380742866
junghoonkim77 <folkball@gmail.com>
2022-08-27 16:00:15 +0900

    Update voc_bank.js

--- java/voc_bank.js
+++ java/voc_bank.js
@@ -1,3 +1,8 @@
-function vocbank(){
-    window.open(this.href, '_blank', 'width=1100, height=900');
+var test_arr = {"평생번호050.html":"평생번호050.html"}
+function vocbank(testarr){
+        window.open(test_arr[testarr], '_blank', 'width=1100, height=900');
 }
+
+/*function vocbank(){
+    window.open(this.href, '_blank', 'width=1100, height=900');
+}*/
