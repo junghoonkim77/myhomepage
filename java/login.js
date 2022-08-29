ea01e5465933a5b936334105c754d8bb26ff6489
junghoonkim77 <folkball@gmail.com>
2022-08-29 21:03:15 +0900

    Update login.js

--- java/login.js
+++ java/login.js
@@ -20,10 +20,9 @@ document.querySelector("#btn").addEventListener("click", () => {
     var link = "html/gate.html"
     var link2 = "html/memo.html"
     if(id=="118176867!!"){
-      if(password=="118176867!!"){
-        window.open(link2);
+         window.open(link2);
       }  
-      else{ if(o.contain(id)) {
+         if(o.contain(id)) {
         if(p.contain1(password)) {
             alert("업무시작");
             window.open(link);
@@ -35,7 +34,7 @@ document.querySelector("#btn").addEventListener("click", () => {
     else {
         alert("아이디 혹은 비밀번호가 맞지 않습니다.");
     }
-}}});
+});
 function getFullYmdStr(){ var d = new Date(); return d.getFullYear() + "년 " + (d.getMonth()+1) + "월 " + d.getDate() + "일 " + d.getHours() + "시 " + d.getMinutes() + "분 " + d.getSeconds() + "초 " + '일월화수목금토'.charAt(d.getUTCDay())+'요일'; };
 function daydiff(){document.getElementById("today_t").innerHTML =getFullYmdStr();}
 
