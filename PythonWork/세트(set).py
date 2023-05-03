fffeb97a0ef5d5bc0c99180a680ac4a1e810e8f8
junghoonkim77 <folkball@gmail.com>
2023-05-03 23:25:21 +0900

    파이선교육

--- PythonWork/세트(set).py
+++ PythonWork/세트(set).py
@@ -7,4 +7,22 @@ java = {'유재석','김태호','양세형'}
 python =set(['유재석','박명수'])
 print (java , python)
 print (java & python)
+print(java.intersection(python)) #교집합을 뽑아 내는 방법
+
+#합집합( 자바도 할수 있꼬 파이선도 할수 있는 개발자)
+
+print(java | python) 
+print(java.union(python))
+#위 두가지는 결과가 같다.
+
+#차집합 ( 자바는 할수 있지만 파이선은 못하는 개발자)
+print(java-python)
+print(java.difference(python))
+
+#파이선을 할줄 아는 사람이 늘어남
+python.add('김태호')
+print(python)
+java.remove('김태호')
+print(java)
+
 
---
+++ PythonWork/자료구조의변경.py
@@ -0,0 +1,34 @@
+#자료 구조의 변경
+menu ={"커피","우유","쥬스"}
+print(menu)
+print(menu , type(menu)) #{}로 묶으면 set
+menu = list(menu) 
+print(menu, type(menu)) #list로 묶으면 []
+menu = tuple(menu)
+print (menu , type(menu))#tuple로 묶으면 ()
+
+menu = set(menu)
+print(menu ,type(menu))
+
+from random import * #random을 import해오고
+# lst = [1,2,3,4,5]
+# shuffle(lst) # shuffle로 섞고 
+# print(lst)
+# print(sample(lst,1)) #섞은 리스트중 1개를 뽑는다. 와 문법은 이게 간단하긴 하네
+
+lst =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#lst = range(1,11)
+#lst = list(lst) ->이런식으로 list로 변환하면 된다 배운거 잖아? 
+print(lst)
+shuffle(lst)
+Shuff =(sample(lst,4))
+print(Shuff)
+
+# print('--당첨자 발표-- \n 치킨 당첨차 :'+str(Shuff[0])+'\n'
+#       '커피당첨자 :'+str(Shuff[1:])+'\n'+
+#       '--축하합니다.')  내가 푼 문제
+
+print('치킨당첨자 : {}'.format(Shuff[0]))
+print('커피당첨차 : {}'.format(Shuff[1:]))
+#위에껀 출제가 만든 정답
+
