5090b6faf2d6f3c68aa696ccacdeeabb6abd63f5
junghoonkim77 <folkball@gmail.com>
2023-05-04 23:23:57 +0900

    파이선 while까지

---
+++ PythonWork/for반복.py
@@ -0,0 +1,17 @@
+# print('대기번호 :1')
+# print('대기번호 :2')
+
+# for waiting_no in [0,1,2,3,4]:  #for in문과 비슷
+#     print('대기번호 : {0}'.format(waiting_no))
+
+# for waiting_no in range(1,5) :   # 1~5직전까지  
+#     print('대기번호 :{}'.format(waiting_no))
+
+
+starbucks = ['아이언맨','토르','아이엠그루트']
+
+for cus in starbucks :
+    print('{},커피가 준비되었습니다.'.format(cus))
+
+
+
