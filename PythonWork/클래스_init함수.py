9655953f782943b9b155b8f618f6c784825b4ddc
junghoonkim77 <folkball@gmail.com>
2023-05-20 23:58:01 +0900

    Update 클래스_init함수.py

--- PythonWork/클래스_init함수.py
+++ PythonWork/클래스_init함수.py
@@ -3,12 +3,22 @@ class Unit :
         self.name = name  #자바스크립트에서의 this가 여기선 파라미터 첫번쨰 se이다.
         self.hp = hp
         self.speed = speed
+        print('{0} 유닛이 생성되었습니다.'.format(name))
+        
 
     def move(self,location):
         print('[지상 유닛 이동]')
         print('{0} : {1} 방향으로 이동합니다. [속도 {2}]'\
               .format(self.name,location,self.speed))
+   
+    def damaged(self,damage) :
+        print('{0} : {1} 데미지를 입었습니다.'.format(self.name, damage))
+        self.hp -= damage
+        print('{0} : 현재 체력은  {1}입니다.'.format(self.name ,self.hp))
+        if self.hp <=0 :
+            print('{0} : 파괴되었습니다.'.format(self.name))
 
+    
 class Attackunit(Unit) :
     def __init__(self,name,hp,speed,damage) :
         Unit.__init__(self,name,hp,speed) #Unit클래스를 상속 받은것 
@@ -18,12 +28,25 @@ class Attackunit(Unit) :
         print('{0} : {1} 방향으로 적군을 공격합니다.공격력 {2}'\
               .format(self.name,location,self.damage)) # 클래스 내부에 선언된 변수를 그대로 활용한다.
         
-    def damaged(self,damage) :
-        print('{0} : {1} 데미지를 입었습니다.'.format(self.name, damage))
-        self.hp -= damage
-        print('{0} : 현재 체력은  {1}입니다.'.format(self.name ,self.hp))
-        if self.hp <=0 :
-            print('{0} : 파괴되었습니다.'.format(self.name))
+class Marine(Attackunit) :
+    def __init__(self) :
+        Attackunit.__init__(self,'마린',40,1,5)
+    
+    #스팀팩 : 일정 시간 동안 이동 및 공격 속도를 증가 , 체력 10 감소 
+
+    def stimpack(self) :
+        if self.hp >10 :
+            self.hp -= 10
+            print('{0} :스팀팩을 사용합니다.(Hp 10 감소)'.format(self.name))
+        else :
+            print('{0} : 체력이 부족해 스팀팩을 사용하지 않습니다.'.format(self.name))
+
+
+#탱크
+class Tank(Att)
+
+
+  
 
 
 #파이어뱃  : 공격유닛 ,화염방사기
