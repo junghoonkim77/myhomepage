71a4786f61fe23ca7b6fe17d2c355d22c5ea69c5
junghoonkim77 <folkball@gmail.com>
2023-08-26 15:06:28 +0900

    상담양식 업데이트

--- PythonWork_basic/1_practice.py
+++ PythonWork_basic/1_practice.py
@@ -1,13 +1,17 @@
 
 name = '김정훈'
-is_human = False
+is_human = False 
+is_human2 = 1>5
 age = 47
 print('풍선')
+print('ㅋ'*7) #문자를 곱할수도 있다.
 print(5>10)
 print(5<10)
 print(False)
-print(not 5>10)
+print(not 5>10) 
 print(name+" 은 "+str(age)+"먹은 병신이다.")
 print(name+ " 은 또라이다.r"+"착할까"+str(is_human))
 # + 대신 , 로 대체할수도 있다. ,로 대체할 경우는 str을 사용하지 않아도 된다. 대신 빈칸이 들어간다.
 print(name ," 은 또라이다.r","착할까" ,is_human)
+print(name, "은 저능아다.그리고 병신같은게 가족들한테 잔소리한다" ,is_human2)
+#애완동물을 소개해 주세요 
