efc88236c8779db41ef23f555ac9019116d6c9f2
junghoonkim77 <folkball@gmail.com>
2023-09-14 23:19:54 +0900

    파이썬

--- PythonWork_basic/16.한줄 for.py
+++ PythonWork_basic/16.한줄 for.py
@@ -1,8 +1,9 @@
 #출석번호
-# students =[] 
-# for student in range(1,5) :
-#   students.append(student)
-#   print(students)
+students =[] 
+for student in range(1,7) :
+  students.append(student)
+ # print(students)
+print(students)
 
 students =[1,2,3,4,5]
 students =[i+100 for i in students] #어우 헷갈려 
@@ -18,6 +19,7 @@ print(students)
 students = [ str(i).upper() for i in students] # 숫자를 upper로 만드니 당연히 에러
 print(students)
 
+#한줄for는 리스트로 묶고 for앞에 나타낼 결과값이 나오게 입력하는 것이다.
 
 
 
