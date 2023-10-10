dfcf686cdbe0fc9d66ed808e0011bb8a1c90da4e
junghoonkim77 <folkball@gmail.com>
2023-10-10 23:29:18 +0900

    파이게임 올리기

--- Pygame_basic/8_frame.py
+++ Pygame_basic/8_frame.py
@@ -1,5 +1,6 @@
 import pygame
-
+########################################################################
+#기본 초기화  ( 반드시 해야 되는것들)
 pygame.init() #초기화
 
 #화면 크기 설정
@@ -46,6 +47,18 @@ enemy_height = enemy_size[1] # 캐릭터의 세로 크기
 enemy_x_pos = (screen_width / 2)-(enemy_width /2) # 화면 가로의 절반 크기에 해당하는 곳에 위치
 enemy_y_pos = screen_height/2 - enemy_height # 화면 세로 크기 가장 아래에 해당하는 곳에 위치
 
+#폰트 정의 
+game_font = pygame.font.Font(None,40) #폰트 객체 생성 ( 폰트, 크기)
+
+#총시간
+total_time = 10
+
+#시간 시간 정보
+start_ticks = pygame.time.get_ticks() #시작 tick을 받아옴
+
+
+
+#이벤트 루프
 running = True #게임이 진행중인가?
 
 while running:
@@ -108,6 +121,24 @@ while running:
     screen.blit(character,(character_x_pos , character_y_pos)) #캐릭터 그리기
     
     screen.blit(enemy,(enemy_x_pos,enemy_y_pos)) #적그리기
+    
+    #타이머 집어 넣기 
+    #경과 시간 계산
+    elapsed_time = (pygame.time.get_ticks() -start_ticks) /1000
+    #경과 시간을 1000으로 나워서 초(s)단위로 표시
+    
+    timer = game_font.render(str(int(total_time- elapsed_time)),True,(255,255,255))
+    #출력할 글자 ,True ,글자 색상
+    screen.blit(timer,(10,10))
+    
+    #만약 시간이 0 이하이면 게임종료
+    if total_time -elapsed_time <= 0 :
+        print('타임아웃')
+        running =False
+    
     pygame.display.update()  #게임 화면을 다시 그리기 
+
+#잠시 대기
+pygame.time.delay(2000) #2초정도 대기
     #pygame 종료
 pygame.quit() # 들여쓰기가 너무 중요해서 더 피곤할수 있다. ㅠㅠ
