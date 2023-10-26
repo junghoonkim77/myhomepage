4a81e02204b8a4f38b0808bd0cd70a7577c8eb1f
junghoonkim77 <folkball@gmail.com>
2023-10-27 00:21:52 +0900

    dd

--- PythonWork_basic/22.파일입출력.py
+++ PythonWork_basic/22.파일입출력.py
@@ -11,39 +11,39 @@
 # score_file.write('\n코딩 : 100점')
 # score_file.close()
 # #--------------------------  파일 읽어오기
+   
+score_file =open('study.txt','r',encoding='utf8')
+print(score_file.read())
+score_file.close()
 
-# score_file =open('score.txt','r',encoding='utf8')
-# print(score_file.read())
-# score_file.close()
+# poem_file =open('poem.hwp','w',encoding='utf8')
+# print('동해물과 백두산이',file=poem_file)
+# print('마르고 닳도록',file=poem_file)
+# print('하느님이 보우하사 우리나라 만세',file=poem_file)
+# print('무궁화 삼천리 화려강산',file=poem_file)
+# print('대한사람 대한으로 길이 보전하세',file=poem_file)
+# poem_file.close()
+# #--------------------------  파일 한줄씩 읽어보기
 
-poem_file =open('poem.hwp','w',encoding='utf8')
-print('동해물과 백두산이',file=poem_file)
-print('마르고 닳도록',file=poem_file)
-print('하느님이 보우하사 우리나라 만세',file=poem_file)
-print('무궁화 삼천리 화려강산',file=poem_file)
-print('대한사람 대한으로 길이 보전하세',file=poem_file)
-poem_file.close()
-#--------------------------  파일 한줄씩 읽어보기
+# # score_file =open('poem.hwp','r',encoding='utf8')
+# # print(score_file.readline())
+# # score_file.close()
 
-# score_file =open('poem.hwp','r',encoding='utf8')
-# print(score_file.readline())
-# score_file.close()
+# #--------------------------  파일 이 몇줄인지 모를때 
+# poem_file =open('poem.hwp','r',encoding='utf8')
 
-#--------------------------  파일 이 몇줄인지 모를때 
-poem_file =open('poem.hwp','r',encoding='utf8')
+# while True :
+#     line = poem_file.readline()
+#     if not line:
+#         break
+#     print(line)
 
-while True :
-    line = poem_file.readline()
-    if not line:
-        break
-    print(line)
+# poem_file.close()
 
-poem_file.close()
+# #-------------------------- 파일을 읽어와 리스트에 저장하는 방법
+# poem_file =open('poem.hwp','r',encoding='utf8')
+# lines = poem_file.readlines() #list형태로 저장
+# for  i in lines :
+#     print(i,end=" ")
 
-#-------------------------- 파일을 읽어와 리스트에 저장하는 방법
-poem_file =open('poem.hwp','r',encoding='utf8')
-lines = poem_file.readlines() #list형태로 저장
-for  i in lines :
-    print(i,end=" ")
-
-poem_file.close()
+# poem_file.close()
