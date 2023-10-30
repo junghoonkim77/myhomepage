3e366ccf2f6f05fe0ec7d28aa2028485ba3b380d
junghoonkim77 <folkball@gmail.com>
2023-10-30 23:15:05 +0900

    파이썬 엑셀

--- python_excel/excel1.py
+++ python_excel/excel1.py
@@ -1,5 +1,12 @@
 import openpyxl
 
-wb = openpyxl.Workbook()
+#wb = openpyxl.Workbook()
 
-print(wb)
+# ws =wb.active
+# ws['a1']=123
+# wb.save('myhomepage\python_excel\테스트.xlsx')
+
+wb = openpyxl.load_workbook('myhomepage\python_excel\테스트.xlsx')
+ws = wb.active
+print(ws['a1'].value)
+
