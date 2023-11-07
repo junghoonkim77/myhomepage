31dbecfc45ff4fc05d2c2531317f01880dd67973
junghoonkim77 <folkball@gmail.com>
2023-11-07 23:17:06 +0900

    파이썬 게임

---
+++ python_guibasic/1.create_frame.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from random import *
+
+root =Tk()
+
+root.title('나도  GUI')
+
+root.geometry('640x480')
+#root.geometry('640x480+100+300') 
+#가로 * 세로  + X좌표 + Y좌표
+#창의 크기를 만듬  X 로 표시해줘야 한다.
+
+root.resizable(True,False)# True는 사이즈 조절가능 , False는 잠금
+
+
+
+
+
+root.mainloop()
