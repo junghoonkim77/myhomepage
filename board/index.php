b364ae40de687184e45621ed480dc90b5a7289fd
junghoonkim77 <folkball@gmail.com>
2024-02-05 16:50:07 +0900

    dd

--- board/index.php
+++ board/index.php
@@ -10,13 +10,7 @@
     <h2>글 목록1</h2>
     <ul>
     <?php 
-    /* $conn = mysqli_connect('localhost','root','amho73032721','abc_corp');
-
-     if(!$conn){
-         echo 'db에 연결하지 못했습니다.'.mysqli_connect_error();
-     } else{
-         echo '데이터 베이스에 접속했습니다.';
-     } */
+    
 
      include("mysqlpass.php");
      // msg_board 테이블에 글조회 데이터를 조회하는건 SELECT이다 
