0664fb55338c33291fc35b07f087216b5b96ab2c
junghoonkim77 <folkball@gmail.com>
2024-02-21 16:28:22 +0900

    Update logout.php

--- 2.4/logout.php
+++ 2.4/logout.php
@@ -1,8 +1,12 @@
 <?php
+//$_SESSION['name'] = '홍길동'
+//unset($_SESSION['name']);
+//$_SESSION['name'] = ''
 session_start();
 session_unset(); // 세션을 비우는것 
-session_destroy();
+session_destroy(); // 
 
 require_once('functions.php');
 redirect('login.php');
+die();
 ?>
