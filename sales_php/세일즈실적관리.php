a53109785e1ea14f975e426c531811003dd841f6
junghoonkim77 <folkball@gmail.com>
2024-02-23 17:05:08 +0900

    Update 세일즈실적관리.php

--- sales_php/세일즈실적관리.php
+++ sales_php/세일즈실적관리.php
@@ -1,4 +1,6 @@
 <?php
+ session_start();
+
 $conn = mysqli_connect('localhost','root','amho73032721','abc_corp');
 
  if(!$conn){
@@ -65,9 +67,15 @@ $conn = mysqli_connect('localhost','root','amho73032721','abc_corp');
             left : 45em;
             bottom : 10px;
         }
-        
-        
+               
     </style>
+
+    
+<script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous">
+</script>
+<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js" integrity="sha256-6XMVI0zB8cRzfZjqKcD01PBsAy3FlDASrlC8SxCpInY=" crossorigin="anonymous">
+</script> 
+
     <title>★통품팀 Sales실적관리창★</title>
 </head>
 <body>
@@ -83,10 +91,11 @@ $conn = mysqli_connect('localhost','root','amho73032721','abc_corp');
                 <td>컨설명</td>
                 <td>상품명</td>
                 <td>고객특이사항_메모</td>
-                <td>삭제❌</td>
+                
             </thead>
+            <tbody id="sales_data">
     <?php 
-      $sql = "SELECT * FROM msg_board";
+      $sql = "SELECT * FROM sales_board";
       $result = mysqli_query($conn,$sql);
       $td = '';
       while($row = mysqli_fetch_array($result)){ 
@@ -94,14 +103,16 @@ $conn = mysqli_connect('localhost','root','amho73032721','abc_corp');
         '<td>'.$row['cusname'].'</td>'.'<td>'.$row['comdate'].'</td>'.'<td>'.$row['hopedate'].'</td>'.
         '<td>'.$row['teamname'].'</td>'.'<td>'.$row['prodname'].'</td>'.'<td>'.$row['spememo'].'</td>';
      }
+     echo $td;
     ?>     
+        </tbody>
         </table>
     </div>
     
     <div class="control">
-      <form action="세일즈실적관리.php" method="post">
+      <form id="myform" action="세일즈실적관리.php" method="post">
       
-        <input placeholder="순번" type="text">
+        
         <input placeholder="고객번호" type="text" name="inum">
         <input placeholder="고객명" type="text" name="cusname">
         <input placeholder="가설일자" type="text" name="comdate">
@@ -114,7 +125,11 @@ $conn = mysqli_connect('localhost','root','amho73032721','abc_corp');
      </form>    
     </div>
     </div>
-       <?php 
+       <?php
+    
+
+
+        $ordernum = $_POST['ordernum'] ?? ''; 
         $inum = $_POST['inum'] ?? ''; // 기본값 설정
         $cusname = $_POST['cusname'] ?? '';
         $comdate = $_POST['comdate'] ?? '';
@@ -122,8 +137,26 @@ $conn = mysqli_connect('localhost','root','amho73032721','abc_corp');
         $teamname = $_POST['teamname'] ?? '';
         $prodname = $_POST['prodname'] ?? '';
         $spememo = $_POST['spememo'] ?? '';
-       // $ordernum = $_POST['ordernum'];
-      
+       
+        if (!empty($inum) && !empty($cusname) && !empty($comdate) && !empty($hopedate) &&
+        !empty($teamname) && !empty($prodname) && !empty($spememo)) {
+
+        $sql1 = "INSERT INTO sales_board (inum, cusname, comdate, hopedate, teamname, prodname, spememo) 
+                VALUES ('$inum', '$cusname', '$comdate', '$hopedate', '$teamname', '$prodname', '$spememo')";
+
+        if (mysqli_query($conn, $sql1)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
+        }
+    } else {
+        echo "All fields are required";
+    };
+
+        
+       
+
+     
        ?> 
     
     <div id="notepad">
@@ -140,67 +173,11 @@ $conn = mysqli_connect('localhost','root','amho73032721','abc_corp');
     <script>
         jQuery(function(){
 
-   
-    var $salesobj = {};
-    var $salesview =[];
-    var $salesDelkey =[];
-      $('.button1').click(function(){
-        $('input[type=text]').each(function(idx){
-        // input의 placeholder값을 가져와서 $salesobj의 키값으로 사용하고 거기에 input의 밸류를 담는다.
+            $(".button1").click(function(){
+                location.reload();
+            })
 
-        var $placehold = $(this).attr('placeholder')
-        var $val = $(this).val();
-        console.log(idx);
-        $salesobj[$placehold ] = $val;
-        }) //여기까지 반복해서 값을  $salesobj 객체에 담는다.
-
-        // key값이 중복되지 않게 고객번호와 순서를 변수에 담는다.
-        var $keyName = $('input[type=text]').eq(2).val() ;
-        var $keySep = $('input[type=text]').eq(0).val() 
-        var  $salesob_str = JSON.stringify ( $salesobj ); //객체값을 str변수로
-         //텍스트 값을 localstorage에 담는다.
-
-        localStorage.setItem(`SA${$keySep}@${$keyName}`,$salesob_str);
-        location.reload();
-             
-      }) // 여기까지가 localstorage에 값 저장하기
-      
-      for ( k in localStorage){
-         if (k.slice(0,2)=="SA"){
-          var Keysa =  localStorage.getItem(k);
-          var $Keysa = JSON.parse (Keysa );
-          $salesview.push($Keysa);
-          
-          }
-       }
-         
-        $salesview.sort((a, b) => a['순번'] - b['순번']);
-        
        
-       
-       // 로컬스토리지 내용 꺼내서 테이블에 보여주기
-      for ( var i=0 ; i<$salesview.length ; i++){
-          
-        $('.thead').before(`<tr class="salesCount">
-            <td>${$salesview[i].순번}</td>
-            <td class="phoneNumber">${$salesview[i].고객번호}</td>
-            <td>${$salesview[i].고객명}</td>
-            <td>${$salesview[i].가설일자}</td>
-            <td>${$salesview[i].설치예정일}</td>
-            <td>${$salesview[i].컨설턴트}</td>
-            <td>${$salesview[i].상품종류}</td>
-            <td>${$salesview[i].고객특이사항}</td>
-            <td class="salesdel" id="${$salesview[i].순번}" data-key="${$salesview[i].고객명}">삭제❌</td>
-            </tr>`)      
-         }  
-         //  로컬스토리지 번호 지우기
-         $('.salesdel').click(function(){
-           var $delKey = $(this).attr('data-key');
-           var $delid = $(this).attr('id');
-           localStorage.removeItem(`SA${$delid}@${$delKey}`)
-           location.reload();
-         }) 
-
           // 휴대폰 번호 복사하기
          $('.phoneNumber').click(function(){
           var $phonenum = $(this).text();
