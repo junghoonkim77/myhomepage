9c807a3da26a2079ef7c47c0398948140c0ba9f9
junghoonkim77 <folkball@gmail.com>
2024-03-16 08:46:17 +0900

    Update 세일즈누적.php

--- sales_php/세일즈누적.php
+++ sales_php/세일즈누적.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style> 
+    .container{
+        display : flex;
+
+    }
      table {
         border : 1px solid gray;
         border-collapse : collapse;
@@ -16,19 +20,24 @@
     <title>세일즈누적</title>
 </head>
 <body>
+    
     <?php
     include('phpgate.php');
     echo '<h2>'.'('.date("Y/m/d").')'."   ".'서울중앙통품 세일즈누적'.'</h2>';
     echo '<a href="세일즈실적관리.php">세일즈실적관리창 이동</a>';
-
+    
+    
     $cusnum = $_GET['cusnum'] ??'';
     $cusname = $_GET['cusname'] ??'';
     $comdate = $_GET['hopedate'] ??'';
     $teamname = $_GET['teamname'] ??'';
     $prodname = $_GET['prodname'] ??'';
-    $sql = "INSERT INTO sales_success(cusnum,cusname,teamname,comdate,prodname)
+
+    if (!empty($cusnum)&&!empty($cusname)&&!empty($comdate)&&!empty($teamname)&&!empty($prodname)){
+        $sql = "INSERT INTO sales_success(cusnum,cusname,teamname,comdate,prodname)
     VALUES('$cusnum','$cusname','$teamname','$comdate','$prodname')" ;
     mysqli_query($conn, $sql);
+      }
 
     $sql1 = "SELECT * FROM sales_success";
     $result = mysqli_query($conn,$sql1);
@@ -40,13 +49,33 @@
     '<td>'.$row['cusname'].'</td>'.'<td>'.$row['teamname'].'</td>'.
     '<td>'.$row['comdate'].'</td>'.'<td>'.$row['prodname'].'</td>'.'</tr>';
     };
+    
+    
 
     ?>
+    <div class="container">
     <table> 
      <td>순서</td><td>고객번호</td><td>고객명</td><td>팀원명</td><td>개통일자</td><td>상품</td>
      
         <?php echo $td ?>
      
     </table>
+    <?php 
+     $delkey = $_POST['delkey']??'';
+
+     if(!empty($delkey)){
+         $sqlDEL = "DELETE FROM sales_success WHERE order_add = $delkey"; 
+         mysqli_query($conn,$sqlDEL);
+         
+     
+    ?>
+   <div class="delinput">
+    <form action="" method="post">
+    <input type="text" name="delkey" placeholder="지울데이터 베이스 번호입력">
+    <input type="submit" value="click">
+   </form>
+   </div>   
+   <?php echo  $delkey.'번이 삭제됐습니다.' ; }; ?>
+</div>  
 </body>
 </html>
