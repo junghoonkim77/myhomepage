cb22946acf45c86ee20a0f38df9caa1d1a27d3e6
junghoonkim77 <folkball@gmail.com>
2024-04-02 11:28:49 +0900

    Update 변수의종류.php

--- php_basic/변수의종류.php
+++ php_basic/변수의종류.php
@@ -10,6 +10,13 @@
     $number = "1";
     echo $number;
     var_dump($number);
-    ?>
+
+    $testarray =array('kim'=>'인생쓰레기','jjoo'=>'완전착하고이쁨','jjunn'=>'착하고순수하고똑똑','eun'=>'착하고순수하고똑똑2');
+    echo $testarray['kim'].'<br>';
+    $testarray2 = ['kim'=>'인생쓰레기','jjoo'=>'완전착하고이쁨','jjunn'=>'착하고순수하고똑똑','eun'=>'착하고순수하고똑똑2'];
+   echo $testarray2['jjoo'].'<br>';
+   echo count($testarray2);
+   
+   ?>
 </body>
 </html>
