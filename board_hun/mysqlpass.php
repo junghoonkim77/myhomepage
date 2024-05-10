78d7552234088fcc17c0aea2a9211fc8960da642
junghoonkim77 <folkball@gmail.com>
2024-05-11 08:45:37 +0900

    dd

--- board_hun/mysqlpass.php
+++ board_hun/mysqlpass.php
@@ -1,9 +1,38 @@
 <?php
-$conn = mysqli_connect('localhost','root','amho73032721','abc_corp');
+// 데이터베이스 정보를 배열로 설정
+$databases = [
+    'folkball.dothome.co.kr' => [
+        'user' => 'folkball',
+        'password' => 'amho73032721!',
+        'dbname' => 'folkball'
+    ],
+    'localhost' => [
+        'user' => 'root',
+        'password' => 'amho73032721',
+        'dbname' => 'abc_corp'
+    ]
+];
 
- if(!$conn){
-     echo 'db에 연결하지 못했습니다.'.mysqli_connect_error();
- } else{
-     echo '데이터 베이스에 접속했습니다.';
- }
+// 서버의 도메인 이름을 가져옴
+$serverName = $_SERVER['SERVER_NAME'];
+
+// 도메인 이름에 따라 데이터베이스 선택
+if (array_key_exists($serverName, $databases)) {
+    $selectedDB = $databases[$serverName];
+} else {
+    die("서버 설정에 맞는 데이터베이스 정보가 없습니다.");
+}
+
+// 선택된 데이터베이스에 대한 연결 시도
+$conn = mysqli_connect('localhost', 
+                       $selectedDB['user'], 
+                       $selectedDB['password'], 
+                       $selectedDB['dbname']);
+
+// 연결 성공 여부 확인
+if (!$conn) {
+    die("연결 실패: " . mysqli_connect_error());
+}
+
+echo "연결 성공";
 ?>
---
+++ php_basic/php문법연습/serverName.php
@@ -0,0 +1,7 @@
+<?php
+// 'SERVER_TYPE' 환경 변수의 값을 가져옵니다.
+$serverName = $_SERVER['SERVER_NAME'];
+
+// 환경 변수의 값을 출력합니다.
+echo "현재 서버 네임: " . $serverName;
+?>
