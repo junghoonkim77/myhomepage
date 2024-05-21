d35b5c91c0859db96eb0b4aa6bd85c72378aa4b8
junghoonkim77 <folkball@gmail.com>
2024-05-21 17:41:00 +0900

    Update json_encode.php

--- code_shelter/php_basic2/json_encode.php
+++ code_shelter/php_basic2/json_encode.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        div{
+            color : blue;
+            font-weight : bold;
+        }
+    </style>
     <title>json_encode연습</title>
 </head>
 <body>
-    
+    <?php 
+    $test ="김정훈입니다.";
+    ?>
+    <div>
+
+    </div>
+  <script>
+    var test = JSON.parse('<?php echo json_encode($test) ?>');
+    document.querySelector('div').innerHTML = test;
+  </script>
 </body>
 </html>
