b0d47c0958708a0519b7a5ba118479b86d6023a6
junghoonkim77 <folkball@gmail.com>
2024-10-13 08:42:16 +0900

    dd

---
+++ code_shelter/php/extention.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+//explode 함수 : 지정된 문자로 문자열을 잘라서 배열을 만들게 도와줌
+$str ="a,b,c,d,e";
+$arr = explode(',',$str);
+print_r($arr);
+
+?>
+
+<script>
+    let arr = [1,2,3,4,5];
+    console.log(arr);
+    let arr1 = arr.join(",");
+    console.log(arr1);
+    console.log(typeof(arr1));
+</script>
+</body>
+</html>
+
--- code_shelter/php/file_read_write.php
+++ code_shelter/php/file_read_write.php
@@ -1,3 +1,14 @@
 <?php 
+$listpath = 'list.txt';
+$listO = fopen($listpath,'r');
+$listSize = filesize($listpath);
+$listR = fread($listO,$listSize);
+echo $listR;
+fclose($listO);
+
+$addCon = '<li><a href="">이건 파일에 추가할내용</a></li>';
+$listO_1 = fopen($listpath,'a'); 
+fwrite($listO_1,$addCon);
+fclose($listO_1);
 
 ?>
