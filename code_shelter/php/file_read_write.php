c1f38a49a1e8681844aa45ee9202cba62b1b069e
junghoonkim77 <folkball@gmail.com>
2024-10-12 09:01:32 +0900

    dd

--- code_shelter/php/file_read_write.php
+++ code_shelter/php/file_read_write.php
@@ -5,13 +5,29 @@ $file = '2.txt';
 
 if(file_exists($file)){
     $pf = fopen($file,'r'); // 읽기모드로 열었음
-    $fz = filesize($file); // 파일사이즈 알아내기
-    $fr = fread($pf,$fz);  //연 파일을 읽기
+    if($pf){
+        $fz = filesize($file); // 파일사이즈 알아내기
+        $fr = fread($pf,$fz);  //연 파일을 읽기
+        if($fr){
+            echo $fr;
+            fclose($pf);
+        }else {
+            echo "파일 읽기에 실패했습니다.";
+        }
+    } else {
+        echo "파일 열기에 실패했습니다.";
+    }
     
-    echo $fr;
+   
 } else{
     echo "파일이 없슈";
 }
  
+// -----------------------------------------------------  밑은 파일 쓰기
 
+$file1 = 'fwrite.txt';
+
+$pf1 = fopen($file1,'w');
+fwrite($pf1,"안녕 정훈아~!");
+fclose($pf1);
 ?>
