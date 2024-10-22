7d1a318b64294fa8dbceb45ba784d6e9d6c388c3
junghoonkim77 <folkball@gmail.com>
2024-10-22 14:35:16 +0900

    dd

---
+++ code_shelter/php/cookies/cookie.php
@@ -0,0 +1,3 @@
+<?php
+echo "쿠키 이름은:".$_COOKIE['pk_name']."입니다";
+?>
---
+++ code_shelter/php/cookies/make.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>쿠키연습</title>
+</head>
+<body>
+    <?php 
+    setcookie('pk_name','김주남',time()+240,'/')
+     ?>
+     <a href="cookie.php">쿠키 이름 찾아가자</a>
+</body>
+</html>
