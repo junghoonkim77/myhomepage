685254a748a441204b43803cf2e958fd8eca2fd3
junghoonkim77 <folkball@gmail.com>
2024-11-03 08:31:12 +0900

    Update filter1.php

--- code_shelter/php/Fileter/filter1.php
+++ code_shelter/php/Fileter/filter1.php
@@ -6,6 +6,10 @@
     <title>입력값 필터</title>
 </head>
 <body>
+    <form action="filter1.php" method="get">
+    <input type="email" name="tel" placeholder="이메일 주소 형식 확인">
+    <button>이메일 주소 형식 확인</button>
+    </form>
     <?php 
     // 정수필터 : FILTER_VALIDATE_INT
     // 실수필터 :FILTER_VALIDATE_FLOAT (정수 필터링)
@@ -21,13 +25,19 @@
     } else{
         echo $i."는 정수가 아닙니다.";
     } */
+    $tel = $_GET['tel']??"";
+    if(filter_var($tel,FILTER_VALIDATE_EMAIL)){
+        echo "이메일 주소가 맞습니다." ;
+    }else {
+        echo "이메일 주소 형식 재확인 필요";
+    }
 
      $int = 100 ;
      $min = 1 ;
      $max = 200 ;
 
      if(filter_var($int,FILTER_VALIDATE_INT,array("options" => array("min_rang" => $min , "max_rang" => $max))) === false){
-        echo "범위를 벗어납니다.";
+        echo "범위를 벗어납니다.";   // 옵션으로 범위를 넣는다.
      }else {
         echo "범위안에 들어와 있습니다.";
      }
