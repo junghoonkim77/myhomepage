12522bdb7cfa86ecc523621314c1d70160d672de
junghoonkim77 <folkball@gmail.com>
2024-11-03 14:01:43 +0900

    php테스트

---
+++ code_shelter/php/github_php/table_create.php
@@ -0,0 +1,13 @@
+<?php
+include('connection.php');
+
+$TableBuild =
+ "CREATE TABLE `githubTest` (
+    `num` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `hobby` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`num`)
+  );" ;
+  
+$result =  mysqli_query($conn,$TableBuild);
+?>
