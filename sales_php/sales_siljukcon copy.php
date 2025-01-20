02dbef82c6765c76cca08b894641582962b612ff
junghoonkim77 <folkball@gmail.com>
2025-01-20 23:53:27 +0900

    이것도 코딩인가? ㅋㅋㅋ

--- sales_php/sales_siljukcon copy.php
+++ sales_php/sales_siljukcon copy.php
@@ -159,7 +159,7 @@ echo '<h2>'.'('.date("Y/m/d").')'."   ".'서울중앙통품 세일즈현황'.'</
     ?> 
        </tbody>
         </table>
-     <?php   echo '<h3>'.'잔여가설건수 : '.$row_count.' 건'.'</h3>'; ?>
+     <?php   echo '<h3 id="excelinsert">'.'잔여가설건수 : '.$row_count.' 건'.'</h3>'; ?>
      <a class="addlink" href="sales_Gate.html"><button>입력창 이동</button></a>
      <a class="addlink" href="sales_nujuk.php"><button>세일즈 누적실적 현황</button></a>
         <?php 
@@ -209,7 +209,8 @@ echo '<h2>'.'('.date("Y/m/d").')'."   ".'서울중앙통품 세일즈현황'.'</
                   ;
            };    
         ?>
-    <div id="notepad">   
+    <div id="notepad">
+       <!--  <button id="excelinsert">복사</button>   -->
         <table id="dailyRepot" style="border:none;">
     
         </table>
@@ -232,16 +233,16 @@ echo '<h2>'.'('.date("Y/m/d").')'."   ".'서울중앙통품 세일즈현황'.'</
     <script>
                         
           // 휴대폰 번호 복사하기 =>잠깐 임시로 주석처리 했음
-         /*$('td').click(function(){
+         $('td').click(function(){
           var $tdtext = $(this).text();
           if(navigator.clipboard){
             $lib.clipcopy($tdtext);
           } else{
             $lib.clipcopy2($tdtext);
           }
-        }) */ 
+        }) 
 
-         // 엑셀파일에 붙여 넣기 코드
+         // 엑셀파일에 붙여 넣을 내용 append하기
 
           $('.dailyexcel').on('click',function(){
            const excel = $(this).siblings("td:nth-child(2)").text();
@@ -251,9 +252,21 @@ echo '<h2>'.'('.date("Y/m/d").')'."   ".'서울중앙통품 세일즈현황'.'</
             console.log(excel1);console.log(excel2);
            $('#dailyRepot').append("<tr><td style='border:none;'>"+excel+"</td>"
             +"<td style='border:none;'>"+excel1+"</td>"
+            +"<td style='border:none;'>"+""+"</td>"
             +"<td style='border:none;'>"+excel2+"</td></tr>");
            })
             
+
+        // 엑셀파일에 붙여넣을 영역 카피하고 내용 비우기
+
+        $('#excelinsert').on('click',function(){
+            $lib.rangecopy('#dailyRepot');
+            $('#dailyRepot').empty();
+            
+        })
+        
+        
+
           // 삭제방지 코드
           $(".delsubmit").click(function(e){
            const $editcheck = $('#edit').prop('checked');
