d2006a91d7d68dce85fa0d63d9b4df1f68c783ad
junghoonkim77 <folkball@gmail.com>
2025-05-08 16:48:10 +0900

    Update tableCRcol.php

--- center2/collect/tableCRcol.php
+++ center2/collect/tableCRcol.php
@@ -2,15 +2,13 @@
 include ('phpgate.php');
 
 // 테이블 생성 SQL
-$sql = "CREATE TABLE cs2collect 
-(teamname VARCHAR(100) PRIMARY KEY,
-simplevoc VARCHAR(10),
-badvoc VARCHAR(10),
-tri_end VARCHAR(10),
-success_end VARCHAR(10),
-todaytime VARCHAR(30)
-
-)";
+$sql = "CREATE TABLE cs2collect (
+  teamname VARCHAR(100) PRIMARY KEY,
+  simplevoc INT,
+  badvoc INT,
+  badofbad INT,
+  vocmemo VARCHAR(100)
+);";
 
 $result =  mysqli_query($conn,$sql);
 ?>
