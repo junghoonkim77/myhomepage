39bf8e90a40f3b191ab7f3d3a531e473593b98cc
junghoonkim77 <folkball@gmail.com>
2025-07-03 16:47:24 +0900

    ddd

---
+++ linkedu/widget_position.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+win = tk.Tk()
+win.geometry("600x300")
+hello = tk.Label(win,text="아빠는 왜이리 병신 같을까? ㅠㅠ",relief='ridge')
+hello.pack(side="left",anchor="n",padx="20",pady="30")
+win.mainloop()
