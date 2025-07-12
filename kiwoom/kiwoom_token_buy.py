0f92f35492cf365dcea0a4214bba993d86e5ffb4
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2025-07-12 09:41:51 +0900

    dd

---
+++ kiwoom/kiwoom_token_buy.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+# 주식 매수주문
+def fn_kt10000(token, data, cont_yn='N', next_key=''):
+	# 1. 요청할 API URL
+	#host = 'https://mockapi.kiwoom.com' # 모의투자
+	host = 'https://api.kiwoom.com' # 실전투자
+	endpoint = '/api/dostk/ordr'
+	url =  host + endpoint
+
+	# 2. header 데이터
+	headers = {
+		'Content-Type': 'application/json;charset=UTF-8', # 컨텐츠타입
+		'authorization': f'Bearer {token}', # 접근토큰
+		'cont-yn': cont_yn, # 연속조회여부
+		'next-key': next_key, # 연속조회키
+		'api-id': 'kt10000', # TR명
+	}
+
+	# 3. http POST 요청
+	response = requests.post(url, headers=headers, json=data)
+
+	# 4. 응답 상태 코드와 데이터 출력
+	print('Code:', response.status_code)
+	print('Header:', json.dumps({key: response.headers.get(key) for key in ['next-key', 'cont-yn', 'api-id']}, indent=4, ensure_ascii=False))
+	print('Body:', json.dumps(response.json(), indent=4, ensure_ascii=False))  # JSON 응답을 파싱하여 출력
+
+# 실행 구간
+if __name__ == '__main__':
+	# 1. 토큰 설정
+	MY_ACCESS_TOKEN = 'yJip6GBnnChYW3KRbHjIhZ9zceVirVE268jrcBJqWGGeRGHTP3jBkclbdwomfkPQB7oSHjbfvuq_PrchJIxrCQ' # 접근토큰
+
+	# 2. 요청 데이터
+	params = {
+		'dmst_stex_tp': 'KRX', # 국내거래소구분 KRX,NXT,SOR
+		'stk_cd': '157450', # 종목코드 
+		'ord_qty': '1', # 주문수량 
+		'ord_uv': '', # 주문단가 
+		'trde_tp': '3', # 매매구분 0:보통 , 3:시장가 , 5:조건부지정가 , 81:장마감후시간외 , 61:장시작전시간외, 62:시간외단일가 , 6:최유리지정가 , 7:최우선지정가 , 10:보통(IOC) , 13:시장가(IOC) , 16:최유리(IOC) , 20:보통(FOK) , 23:시장가(FOK) , 26:최유리(FOK) , 28:스톱지정가,29:중간가,30:중간가(IOC),31:중간가(FOK)
+		'cond_uv': '', # 조건단가 
+	}
+
+	# 3. API 실행
+	fn_kt10000(token=MY_ACCESS_TOKEN, data=params)
+
+	# next-key, cont-yn 값이 있을 경우
+	# fn_kt10000(token=MY_ACCESS_TOKEN, data=params, cont_yn='Y', next_key='nextkey..')
