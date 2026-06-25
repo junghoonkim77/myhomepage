7728e52e5be7aeb3cb8c30b1d9f20f52bcc1bf15
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2026-06-25 21:24:37 +0900

    통품voc은행 업데이트

--- center2/srsearch/index.php
+++ center2/srsearch/index.php
@@ -140,7 +140,7 @@ $result = $conn->query($sql);
             $('#vocSearch').trigger('keyup');
         });
         // 버전 정보 표시
-        const nowversion = "사례집_ver.28";
+        const nowversion = "사례집_ver.30";
         $('#nowversion').text(nowversion);
     });
     </script>
