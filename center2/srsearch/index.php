d3b590ed96f68ba7944172d614ae8122a360fc99
junghoonkim77 <111161283+junghoonkim77@users.noreply.github.com>
2026-07-22 22:30:55 +0900

    Update index.php

--- center2/srsearch/index.php
+++ center2/srsearch/index.php
@@ -140,7 +140,7 @@ $result = $conn->query($sql);
             $('#vocSearch').trigger('keyup');
         });
         // 버전 정보 표시
-        const nowversion = "사례집_ver.39";
+        const nowversion = "사례집_ver.40";
         $('#nowversion').text(nowversion);
     });
     </script>
